a04cc64af73d320016db793b8709b3bd8baf01dc
iza <kan077bct039@kec.edu.np>
2023-11-03 13:24:07 +0545

    edit function added

--- routes/books.js
+++ routes/books.js
@@ -7,10 +7,20 @@ router.get('/add', function(req, res, next) {
   res.render('addBooks', { title: 'Add Books' });
 });
 
-module.exports = router;
-
 router.post('/save',function(req,res,next){
   books.push({...req.body, _id:`00${books.length + 1}`})
   res.redirect('/')
 })
 
+router.get('/edit/:_id', function(req, res, next){
+  const book = books.find((book)=>book._id === req.params._id)
+  res.render('editBooks', {title: "Edit Books", book})
+})
+
+router.post('/saveEdited/:_id',function(req,res,next){
+  const currIndex = books.findIndex((book)=>book._id === req.params._id)
+  books.splice(currIndex, 1, {...req.body, _id: req.params._id})
+  res.redirect('/')
+})
+
+module.exports = router;
