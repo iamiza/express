ba8b0c3c136eb4b871c263737f2bb2be1e27d8be
iza <kan077bct039@kec.edu.np>
2023-11-03 12:10:33 +0545

    add disply for books

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var books = require('../resources/books')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Book App', bookList:books });
+  res.render('index', { title: 'Book App' });
 });
 
 module.exports = router;
