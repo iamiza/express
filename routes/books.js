f2b00b383161b601ff3e900c82e5545a52bd08c2
iza <kan077bct039@kec.edu.np>
2023-11-04 14:06:16 +0545

    connected database

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var books = require("../resources/books")
-
+//var books = require("../resources/books")
+const Books = require('../models/books')
 
 router.get('/add', function(req, res, next) {
   res.render('addBooks', { title: 'Add Books' });
+
 });
 
-router.post('/save',function(req,res,next){
-  books.push({...req.body, _id:`00${books.length + 1}`})
+router.post('/save',async function(req,res,next){
+  //books.push({...req.body, _id:`00${books.length + 1}`})
+  await Books.insertMany([req.body]);
   res.redirect('/')
 })
 
-router.get('/edit/:_id', function(req, res, next){
+/*router.get('/edit/:_id', function(req, res, next){
   const book = books.find((book)=>book._id === req.params._id)
   res.render('editBooks', {title: "Edit Books", book})
 })
@@ -31,6 +33,7 @@ router.get('/deleteBooks/:_id', function(req,res,next){
   
   } 
   res.redirect('/')
-})
+  
+})*/
 
 module.exports = router;
